8d3ca97b2c730040a21fd0d96747509a0c30cf0b
guozeng <331323923@qq.com>
2022-07-02 22:41:08 +0800

    prettier & editorconfig

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  bracketSameLine: false,
+  singleAttributePerLine: false,
+  htmlWhitespaceSensitivity: 'ignore',
+  arrowParens: 'always',
+}
